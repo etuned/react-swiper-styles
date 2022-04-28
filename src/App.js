6ff78d1365b95bdf894e861f7380da99d310b52b
Edwin Bartunek <edwin@devhart.io>
2022-04-29 00:56:53 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+import { Swiper, SwiperSlide } from "swiper/react";
 
-function App() {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./App.css"
+
+// import required modules
+import { Navigation } from "swiper";
+
+const Wrapper = styled.div`
+width: 100vw;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const Container = styled.div`
+max-width: 500px;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+@media (max-width: 768px) {
+  width: 100%;
+}
+`
+const Title = styled.h1`
+text-align: center;
+
+`
+const SubTitle = styled.h3`
+text-align: center;
+`
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Container>
+        <Title>Title</Title>
+        <SubTitle>Subtitle</SubTitle>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </Container>
+    </Wrapper>
   );
 }
-
-export default App;
